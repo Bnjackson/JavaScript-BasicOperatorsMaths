3b20b9d33d7d6e5228b105527a49203323613202
Bnjackson <65509144+Bnjackson@users.noreply.github.com>
2020-07-12 14:03:13 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+BASIC OPERATORS MATHS
+Operand or agruments - is what opertors are applied to. For instance in 5 * 2 there are two operands the left operand is 5 and the right operand is 2.
+
+An operator is unary if it has a single operand
+
+An operator is binary if it has two operands.
+
+MATHS
+Addition +
+Subtraction -
+Multiplication *
+Division /
+Remainder %
+Exponeration **
+
+
+*/
